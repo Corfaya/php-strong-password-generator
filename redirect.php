4f8725cce9892903451b7adce882947f5ddbf4b1
Corfaya <lauracorrado0696@gmail.com>
2024-09-06 17:41:45 +0200

    [UPD] style fixed

--- redirect.php
+++ redirect.php
@@ -13,8 +13,9 @@ if (!isset($_SESSION["user_password"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>La tua password</title>
 </head>
 
@@ -28,13 +29,12 @@ if (!isset($_SESSION["user_password"])) {
             </div>
             <div class="row">
                 <div class="col-12">
-                    <div class="col-12">
-                        <h2 class="text-center">La tua password sicura è:</h2>
+                        <h2 class="text-center my-3">La tua password sicura è:</h2>
                         <div class="alert alert-success" role="alert">
                             <?php echo $_SESSION["user_password"] ?>
                         </div>
 
-                    </div>
+                   
 
                 </div>
             </div>
